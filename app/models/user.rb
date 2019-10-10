719f7b75a2b39fc82ea244551f3940bb57d430d2
github username <github email address>
2019-10-10 13:05:56 -0500

    Added modified user model and added serializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :user_times
+    def isDateDuplicated?(date)
+   
+        self.user_times.none?{ |day_time| day_time.clock_in.to_s.slice(8, 2) == date }
+
+    end
 end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,6 @@
+class UserSerializer
+    include FastJsonapi::ObjectSerializer
+    attributes :name
+    has_many :user_times
+  end
+  
